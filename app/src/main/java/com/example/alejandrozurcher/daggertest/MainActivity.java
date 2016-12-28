10b97e00f96c412c76a68cbcff2e0053fcc64fca
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2016-12-28 10:07:43 -0300

    Adding Butterknife and Retrofit

--- app/src/main/java/com/example/alejandrozurcher/daggertest/MainActivity.java
+++ app/src/main/java/com/example/alejandrozurcher/daggertest/MainActivity.java
@@ -1,23 +1,39 @@
 package com.example.alejandrozurcher.daggertest;
 
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import java.util.Arrays;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String USER_NAME_KEY = "USER_NAME_KEY";
 
     @Inject
     ConnectivityManager manager;
+    @Inject
+    SharedPreferences sharedPreferences;
+
+    @BindView(R.id.network_text)
+    TextView network_text;
+    @BindView(R.id.user_text)
+    TextView user_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
 
         //Inject dependencies to MainActivity
         ((App) getApplication()).getComponent().inject(this);
@@ -27,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("Active Network: " + manager.getActiveNetwork());
             System.out.println("Active Network Info: " + manager.getActiveNetworkInfo());
             System.out.println("Networks: " + Arrays.toString(manager.getAllNetworks()));
+
+            network_text.setText(manager.getActiveNetworkInfo().toString());
         }
+
+        //Using injected SharedPreferences
+        sharedPreferences.edit().putString(USER_NAME_KEY, "zurche").apply();
+
+        //Read preference
+        user_text.setText(sharedPreferences.getString(USER_NAME_KEY, ""));
     }
 }
--- app/src/main/java/com/example/alejandrozurcher/daggertest/module/ApplicationModule.java
+++ app/src/main/java/com/example/alejandrozurcher/daggertest/module/ApplicationModule.java
@@ -1,6 +1,7 @@
 package com.example.alejandrozurcher.daggertest.module;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 
 import javax.inject.Singleton;
@@ -30,6 +31,12 @@ public class ApplicationModule {
     @Singleton
     public ConnectivityManager providesConnectivityManager(Context context) {
         return (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    @Provides
+    @Singleton
+    public SharedPreferences providesSharedPreferences(Context context) {
+        return context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
     }
 
 }
